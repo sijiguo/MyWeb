095d9b9e02bd43719151c02e7824220920bd8c5d
Administrator <Administrator@USER-20151129UT>
2016-01-19 22:02:46 +0800

    12345

--- src/com/java/controller/LoginController.java
+++ src/com/java/controller/LoginController.java
@@ -1,12 +1,13 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("login")
 public class LoginController {
-	public String Query(){
+	public String Query(@ModelAttribute String id){
 		return "index.jsp";
 	}
 }
